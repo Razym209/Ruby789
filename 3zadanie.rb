b09d350a42838f5b8a1eb0739c15e1f560984ece
Razym209 <47150855+Razym209@users.noreply.github.com>
2019-02-08 19:48:29 +0300

    Update 3zadanie.rb

--- 3zadanie.rb
+++ 3zadanie.rb
@@ -7,34 +7,23 @@ b = gets.chomp.to_i
 puts "3 сторона треугольника"
 c = gets.chomp.to_i
 
-if a > b  && a > c
+if a > b && a > c
   boldlin = a
-
-  if (boldlin**2) - (b**2) + (c**2) == 0
-    puts "треугольник прямоугольный"
-  else (boldlin**2) - (b**2) + (c**2) != 0
-    puts "треугольник непрямоугольный"
-  end
-
-elsif c > b  && c > a
-  boldlin = c
- 
-  if (boldlin**2) - (a**2) + (b**2) == 0
-    puts "треугольник прямоугольный"
-  else (boldlin**2) - (a**2) + (b**2) != 0
-    puts "треугольник непрямоугольный"
-  end
-
-elsif b > a  && b > c
+  lin1 = b
+  lin2 = c
+elsif b > a && b > c
   boldlin = b
- 
- 	if (boldlin**2) - (a**2) + (c**2) == 0
- 		puts "треугольник прямоугольный"
-	else (boldlin**2) - (a**2) + (c**2) != 0
-  		puts "треугольник непрямоугольный"
-	end
+  lin1 = a
+  lin2 = c
+elsif c > b && c > a
+  boldlin = c
+  lin1 = a
+  lin2 = b
+end
 
-else
+if (boldlin**2) - (lin1**2) + (lin2**2) == 0
+   puts "треугольник прямоугольный"
+else (boldlin**2) - (lin1**2) + (lin2**2) != 0
   puts "треугольник непрямоугольный"
 end
 
