1cf6c1c0dff73b0997bee381454206113b3e74d4
Razym209 <47150855+Razym209@users.noreply.github.com>
2019-02-16 18:57:16 +0300

    Delete 6zadanie.rb

--- 6zadanie.rb
+++
@@ -1,5 +0,0 @@
-God = { 'January' => 31,   'May' => 31,       'September' => 30, 'February' => 28,
-        'June' => 30,      'October' => 31,   'March' => 31,     'July' => 31, 
-        'November' => 30,  'April' => 30,     'August' => 31,    'December' => 31 }
-
-God.each { |mesiz, den| puts mesiz if den == 30 }
