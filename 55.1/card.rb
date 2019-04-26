5a63cd63e3ee764c21bbde70c4beef39b3f515a4
Razym209 <47150855+Razym209@users.noreply.github.com>
2019-04-26 12:52:04 +0300

    Add files via upload

---
+++ 55.1/card.rb
@@ -0,0 +1,23 @@
+class Card
+  attr_reader :suit, :value
+
+  SUITS = %w[♢ ♧ ♤ ♡].freeze
+  CARD = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+  DEFAULT_VALUE = 10
+  ACE_MIN_VALUE = 1
+
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
+  end
+
+  def get_card_score(card)
+    if %w[J Q K].include? card.value
+      DEFAULT_VALUE
+    elsif card.value == 'A'
+      ACE_MIN_VALUE
+    else
+      card.value.to_i
+    end
+  end
+end
